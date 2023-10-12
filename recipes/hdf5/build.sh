57d5c7fa5faa5744d982817fd6f82a9549a1be7a
David Andrs <andrsd@gmail.com>
2023-10-12 09:23:50 -0600

    hdf5: adding version 1.12.1

---
+++ recipes/hdf5/build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -x
+
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/libtool/build-aux/config.* ./bin
+
+export CC=mpicc
+export CXX=mpic++
+export FC=mpifort
+
+./configure --prefix="${PREFIX}" \
+            --enable-parallel \
+            --with-pic \
+            --host="${HOST}" \
+            --build="${BUILD}" \
+            --with-zlib="${BUILD_PREFIX}" \
+            --with-pthread=yes \
+            --with-default-plugindir="${PREFIX}/lib/hdf5/plugin" \
+            --enable-build-mode=production \
+            --enable-using-memchecker \
+            --enable-shared=yes \
+            --enable-static=no \
+            --enable-ros3-vfd \
+            --enable-tests=no
+
+make -j "${CPU_COUNT}" ${VERBOSE_AT}
+make install
