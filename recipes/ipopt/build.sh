08e32240c9743e104408d957f5c76bb13bb251c3
David Andrs <andrsd@gmail.com>
2024-03-23 11:42:51 -0600

    ipopt: version 3.14.14

---
+++ recipes/ipopt/build.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/gnuconfig/config.* .
+
+cd $SRC_DIR
+
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib"
+export SPRAL_OPTIONS=
+if [ "$(uname)" == "Linux" ]; then
+  export LDFLAGS="${LDFLAGS} -lrt"
+  export SPRAL_OPTIONS="--with-spral --with-spral-cflags=-I${PREFIX}/include --with-spral-lflags=-lspral"
+fi
+
+mkdir build
+cd build
+
+../configure \
+  --without-hsl $SPRAL_OPTIONS \
+  --disable-java \
+  --with-mumps \
+  --with-mumps-cflags="-I${PREFIX}/include/mumps_seq" \
+  --with-mumps-lflags="-ldmumps_seq -lmumps_common_seq -lpord_seq -lmpiseq_seq -lesmumps -lscotch -lscotcherr -lmetis -lgfortran" \
+  --with-asl \
+  --with-asl-cflags="-I${PREFIX}/include/asl" \
+  --with-asl-lflags="-lasl" \
+  --prefix=${PREFIX} || cat config.log
+
+make -j${CPU_COUNT}
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
+  # Environment variables needed by spral
+  # See https://github.com/ralna/spral#usage-at-a-glance
+  export OMP_CANCELLATION=TRUE
+  export OMP_PROC_BIND=TRUE
+  make test
+fi
+make install
+
+# for backward compatibility
+install -d ${PREFIX}/include/coin
+install -m644 ${PREFIX}/include/coin-or/* ${PREFIX}/include/coin
