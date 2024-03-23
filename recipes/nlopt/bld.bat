93d937d414afcce29400d969ac9af13328a673b1
David Andrs <andrsd@gmail.com>
2024-03-23 06:53:39 -0600

    nlopt: version 2.7.1

---
+++ recipes/nlopt/bld.bat
@@ -0,0 +1,21 @@
+
+mkdir build && cd build
+
+set CMAKE_CONFIG="Release"
+
+cmake -LAH -G"NMake Makefiles"                     ^
+  -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%"           ^
+  -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"        ^
+  -DINSTALL_PYTHON_DIR="%SP_DIR%" ..
+if errorlevel 1 exit 1
+
+cmake --build . --config %CMAKE_CONFIG% --target install
+if errorlevel 1 exit 1
+
+copy nlopt.dll test
+ctest --output-on-failure --timeout 100
+if errorlevel 1 exit 1
+
+set DIST_INFO_PATH=%SP_DIR%\%PKG_NAME%-%PKG_VERSION%.dist-info
+mkdir %DIST_INFO_PATH%
+copy nul %DIST_INFO_PATH%\METADATA
