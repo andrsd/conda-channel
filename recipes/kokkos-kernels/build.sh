bdd5174a3f565003b2fe332e256a322ca6aded71
David Andrs <andrsd@gmail.com>
2024-08-04 19:24:37 -0600

    kokkos-kernels: version 4.1.00, build 0

---
+++ recipes/kokkos-kernels/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -x
+
+mkdir build
+cd build
+cmake ${CMAKE_ARGS} \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_SHARED_LIBS=ON \
+    -DBUILD_TESTING=OFF \
+    -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=ON \
+    -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON \
+    ${SRC_DIR}
+
+make -j${CPU_COUNT}
+make install
