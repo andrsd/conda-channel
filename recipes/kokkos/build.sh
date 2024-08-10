2c2cd534099f7435f53862cb3b18c1474fa03e9f
David Andrs <andrsd@gmail.com>
2024-08-10 17:37:36 -0600

    kokkos: version 4.1.00, build 5
    - use libdl
    - strip -fvisibility-inlines-hidden from CXXFLAGS

--- recipes/kokkos/build.sh
+++ recipes/kokkos/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -x
 
+export CXXFLAGS="`echo $CXXFLAGS | sed 's/-fvisibility-inlines-hidden//'`"
+
 mkdir build
 cd build
 cmake ${CMAKE_ARGS} \
@@ -10,13 +12,23 @@ cmake ${CMAKE_ARGS} \
     -DKokkos_ENABLE_COMPLEX_ALIGN=OFF \
     -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=ON \
     -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON \
-    -DKokkos_ENABLE_MPI=ON \
     -DKokkos_ENABLE_HWLOC=ON \
     -DKokkos_ENABLE_SERIAL=ON \
     -DKokkos_ENABLE_LIBDL=OFF \
     -DKokkos_ENABLE_OPENMP=ON \
     -DCMAKE_CXX_STANDARD=17 \
     ${SRC_DIR}
+
+sedinplace() {
+    if [[ $(uname) == Darwin ]]; then
+        sed -i "" "$@"
+    else
+        sed -i"" "$@"
+    fi
+}
+
+sedinplace s%${BUILD_PREFIX}%${PREFIX}%g temp/kokkos_launch_compiler
+sedinplace s%${BUILD_PREFIX}%${PREFIX}%g KokkosConfigCommon.cmake
 
 make -j${CPU_COUNT}
 make install
