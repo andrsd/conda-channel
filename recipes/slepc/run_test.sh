fa37bb5a4533557b2a21d0c9a88000af0baefe39
David Andrs <andrsd@gmail.com>
2025-01-01 14:07:50 -0700

    slepc 3.21.2, build 0

--- recipes/slepc/run_test.sh
+++ recipes/slepc/run_test.sh
@@ -10,10 +10,8 @@ export PETSC_DIR=${PREFIX}
 export SLEPC_DIR=${PREFIX}
 cd "tests"
 make test10
-make test14f
 
 # FIXME: Workaround mpiexec setting O_NONBLOCK in std{in|out|err}
 # See https://github.com/conda-forge/conda-smithy/pull/337
 # See https://github.com/pmodels/mpich/pull/2755
 make runtest10  MPIEXEC="${RECIPE_DIR}/mpiexec.sh"
-make runtest14f MPIEXEC="${RECIPE_DIR}/mpiexec.sh"
