fcdde0abe904c5b02e04ae19aff07fc27589c507
David Andrs <andrsd@gmail.com>
2025-01-29 10:37:00 -0700

    libocca: version 2.0.0, build 0

--- recipes/libocca/build.sh
+++ recipes/libocca/build.sh
@@ -1,38 +1,19 @@
 #!/bin/bash
+set -ex
 
-if [[ "$target_platform" == osx-* ]]
-then
-  # force the use of Apple compiler
-  export CC=/usr/bin/clang
-  export CXX=/usr/bin/clang++
-  unset FC
-  CONF_OPTS=(
-    "-DENABLE_METAL=ON"
-    "-DENABLE_OPENCL=ON"
-    "-DENABLE_OPENMP=OFF"
-    "-DENABLE_DPCPP=OFF"
-    "-DENABLE_CUDA=OFF"
-    "-DENABLE_HIP=OFF"
-  )
-elif [[ "$target_platform" == linux-* ]]
-then
-  CONF_OPTS=(
-    "-DENABLE_METAL=OFF"
-    "-DENABLE_OPENCL=ON"
-    "-DENABLE_OPENMP=ON"
-    "-DENABLE_DPCPP=OFF"
-    "-DENABLE_CUDA=OFF"
-    "-DENABLE_HIP=OFF"
-  )
-fi
-
-cmake -S ${SRC_DIR} -B build \
+cmake ${CMAKE_ARGS} \
+  -S ${SRC_DIR} -B build \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
   -DCMAKE_BUILD_TYPE=Release \
-  -DENABLE_FORTRAN=OFF \
-  -DENABLE_TESTS=OFF \
-  -DENABLE_EXAMPLES=OFF \
-  ${CONF_OPTS[@]}
+  -DOCCA_ENABLE_METAL=OFF \
+  -DOCCA_ENABLE_OPENCL=OFF \
+  -DOCCA_ENABLE_OPENMP=ON \
+  -DOCCA_ENABLE_DPCPP=OFF \
+  -DOCCA_ENABLE_CUDA=OFF \
+  -DOCCA_ENABLE_HIP=OFF \
+  -DOCCA_ENABLE_FORTRAN=OFF \
+  -DOCCA_ENABLE_TESTS=OFF \
+  -DOCCA_ENABLE_EXAMPLES=OFF
 
 cmake --build build --parallel ${CPU_COUNT}
 cmake --build build -- install
