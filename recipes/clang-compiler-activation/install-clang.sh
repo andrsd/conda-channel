05bbada9023a4f8441c9a8907c5e0f5c6244aae4
David Andrs <andrsd@gmail.com>
2025-05-01 12:39:56 -0600

    clang-compiler-activation: version 18.1.8, build 18

--- recipes/clang-compiler-activation/install-clang.sh
+++ recipes/clang-compiler-activation/install-clang.sh
@@ -4,13 +4,13 @@ set -e -x
 
 CHOST=${macos_machine}
 
-mkdir -p "${PREFIX}"/etc/conda/{de,}activate.d/
-cp "${SRC_DIR}"/activate-clang.sh "${PREFIX}"/etc/conda/activate.d/activate_"${PKG_NAME}".sh
-cp "${SRC_DIR}"/deactivate-clang.sh "${PREFIX}"/etc/conda/deactivate.d/deactivate_"${PKG_NAME}".sh
-
 pushd "${PREFIX}"/bin
   ln -s clang ${CHOST}-clang
-  if [[ "${CBUILD}" != ${CHOST} ]]; then
+  ln -s clang-cpp ${CHOST}-clang-cpp
+  if [[ "${CBUILD}" != ${CHOST} ]] && [[ "${target_platform}" != linux-* ]]; then
+    # on linux, the `clang` package already has a $TRIPLE-clang, see
+    # https://github.com/conda-forge/clangdev-feedstock/pull/251
     ln -s clang ${CBUILD}-clang
+    ln -s clang-cpp ${CBUILD}-clang-cpp
   fi
 popd
