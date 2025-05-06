830a589c1545561832ac67903babe2c372ff68d8
David Andrs <andrsd@gmail.com>
2025-05-06 07:31:54 -0600

    libfabric: version 1.19.1, build 3

---
+++ recipes/libfabric/build.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -ex
+
+# verify ABI version
+CURRENT_ABI=$(cat libfabric.map.in| grep -o '^FABRIC_[[:digit:]\.]\+' | tail -n 1)
+echo "CURRENT_ABI=${CURRENT_ABI}"
+
+if [[ "$CURRENT_ABI" != "FABRIC_$LIBFABRIC_ABI" ]]; then
+  echo "CURRENT_ABI=${CURRENT_ABI} != FABRIC_${$LIBFABRIC_ABI}"
+  exit 1
+fi
+
+build_with_libnl=""
+if [[ "$target_platform" == linux-* ]]; then
+  echo "Build with libnl support"
+  build_with_libnl=" --with-libnl=$PREFIX "
+fi
+
+if [[ "$target_platform" == osx-arm64 ]]; then
+  # fix outdated config.sub on mac arm
+  echo 'echo arm64-apple-darwin' > config/config.sub
+fi
+
+./configure \
+    --prefix=$PREFIX \
+    --docdir=$PWD/noinst/doc \
+    --mandir=$PWD/noinst/man \
+    $build_with_libnl \
+    --disable-static \
+    --disable-psm3 \
+    --disable-opx
+
+make -j"${CPU_COUNT}"
+
+if [[ "$target_platform" == linux-* ]]; then
+  make check
+fi
+
+make install
