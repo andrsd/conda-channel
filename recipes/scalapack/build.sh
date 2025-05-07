3c6425a15556c034ef768254f87bd605ce558608
David Andrs <andrsd@gmail.com>
2025-05-07 10:43:03 -0600

    scalapack: version 2.2.1, build 2

--- recipes/scalapack/build.sh
+++ recipes/scalapack/build.sh
@@ -4,17 +4,33 @@ export CC=mpicc
 export CXX=mpic++
 export FC=mpifort
 
-CMAKE_BUILD_TYPE=Release
+# see https://github.com/Reference-ScaLAPACK/scalapack/issues/31
+export CFLAGS="${CFLAGS} -Wno-error=implicit-function-declaration"
+# Workaround for https://github.com/conda-forge/scalapack-feedstock/pull/30#issuecomment-1061196317
+export FFLAGS="${FFLAGS} -fallow-argument-mismatch"
+
+cmake_args=(
+    ${CMAKE_ARGS}
+    -DCMAKE_PREFIX_PATH=${BUILD_PREFIX}
+    -DCMAKE_INSTALL_PREFIX=${PREFIX}
+    -DCMAKE_BUILD_TYPE=Release
+    -DBUILD_SHARED_LIBS=ON
+    -DSCALAPACK_BUILD_TESTS=OFF
+)
+
+if [[ "$target_platform" == linux-* ]]; then
+    cmake_args+=(
+        -DTPL_BLAS_LIBRARIES=openblas
+        -DTPL_LAPACK_LIBRARIES=openblas
+    )
+elif [[ "$target_platform" == osx-* ]]; then
+    cmake_args+=(
+        -DTPL_BLAS_LIBRARIES=blas
+        -DTPL_LAPACK_LIBRARIES=lapack
+    )
+fi
 
 mkdir build
 cd build
-cmake ${CMAKE_ARGS} \
-  -DCMAKE_PREFIX_PATH=${BUILD_PREFIX} \
-  -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-  -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} \
-  -DBUILD_SHARED_LIBS=ON \
-  -DSCALAPACK_BUILD_TESTS=OFF \
-  -DLAPACK_LIBRARIES=openblas \
-  -DBLAS_LIBRARIES=openblas \
-  ${SRC_DIR}
+cmake "${cmake_args[@]}" ${SRC_DIR}
 make install -j${CPU_COUNT}
