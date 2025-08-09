5a185bfcdbc501504673b943600d2934ac951d4f
David Andrs <andrsd@gmail.com>
2025-08-09 05:52:38 -0600

    fixup! boost: adding support for python 3.12

--- recipes/boost/build.sh
+++ recipes/boost/build.sh
@@ -71,7 +71,7 @@ mkdir temp_prefix
     runtime-link=shared \
     link=shared \
     toolset=${TOOLSET} \
-    python="${PY_DUMMY_VER}" \
+    python="${PY_VER}" \
     include="${INCLUDE_PATH}" \
     cxxflags="${CXXFLAGS}" \
     linkflags="${LINKFLAGS}" \
