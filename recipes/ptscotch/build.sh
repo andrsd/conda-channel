f82394359ec2bd892d539eb7aff2e03dcea3ebeb
David Andrs <andrsd@gmail.com>
2025-12-06 06:39:46 -0700

    ptscotch: version 7.0.6, build 0

--- recipes/ptscotch/build.sh
+++ recipes/ptscotch/build.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
-set -ex
+set -x
 
-cp $RECIPE_DIR/files/Makefile.inc ${SRC_DIR}/src/Makefile.inc
+cmake_args=(
+    -DINTSIZE:STRING=64
+    -DINSTALL_METIS_HEADERS:BOOL=OFF
+    -DSCOTCH_METIS_PREFIX:BOOL=ON
+    -DCOMMON_FILE_COMPRESS_GZ:BOOL=ON
+    -DBUILD_FORTRAN:BOOL=OFF
+    -DENABLE_TESTS:BOOL=OFF
+)
 
-export CFLAGS="${CFLAGS} -O3 -fPIC -DCOMMON_FILE_COMPRESS_GZ -DCOMMON_RANDOM_FIXED_SEED -DSCOTCH_RENAME -Drestrict=\"restrict\" -DINTSIZE64 -DSCOTCH_METIS_PREFIX -DSCOTCH_RENAME -DINTSIZE64 -DSCOTCH_METIS_PREFIX"
-export CFLAGS="${CFLAGS} -lz -std=c11"
-
-if [[ "$(uname)" == "Darwin" ]] ; then
-  export CFLAGS="${CFLAGS} -DCOMMON_TIMING_OLD -DCOMMON_OS_MACOS"
+if [[ "$target_platform" == osx-* ]]; then
+    cmake_args+=(
+        -DCOMMON_TIMING_OLD:BOOL=ON
+    )
 fi
 
-export LDFLAGS="${LDFLAGS} -lz -lm"
-
-if [[ "$(uname)" == "Linux" ]] ; then
-  export LDFLAGS="${LDFLAGS} -lrt"
-fi
-
-
-# Scotch has a file identical to one in ParMETIS, remove it so ParMETIS will not use it by mistake
-rm -f ${SRC_DIR}/include/parmetis.h
-# This would only be produced if "make scotch" was invoked, but try to remove it anyway in case someone was messing
-# with it
-rm -f ${SRC_DIR}/include/metis.h
-
-cd ${SRC_DIR}/src
-make ptesmumps esmumps
-make prefix=${PREFIX} install
+cmake \
+    -S ${SRC_DIR} \
+    -B build \
+    ${CMAKE_ARGS} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_PREFIX_PATH=${PREFIX} \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    "${cmake_args[@]}"
+cmake --build build --parallel ${CPU_COUNT}
+cmake --build build -- install
