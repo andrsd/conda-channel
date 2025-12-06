4ffaf6035aaa34f0c5e8b55979d70e7682a7bafd
David Andrs <andrsd@gmail.com>
2025-12-06 07:18:47 -0700

    krado: version 2.3.0, build 1

--- recipes/krado/build.sh
+++ recipes/krado/build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-cmake -S . -B build ${CMAKE_ARGS} \
-  -DCMAKE_FIND_ROOT_PATH="${PREFIX};${BUILD_PREFIX};${BUILD_PREFIX}/${HOST}/sysroot" \
-  -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-  -DCMAKE_BUILD_TYPE=Relase \
-  -DBUILD_SHARED_LIBS=ON \
-  ${SRC_DIR}
-
-cmake --build build --parallel ${CPU_COUNT}
+cmake \
+    -S ${SRC_DIR} \
+    -B build \
+    ${CMAKE_ARGS} \
+    -DCMAKE_FIND_ROOT_PATH="${PREFIX};${BUILD_PREFIX};${BUILD_PREFIX}/${HOST}/sysroot" \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    -DCMAKE_BUILD_TYPE=Relase \
+    -DBUILD_SHARED_LIBS=ON
+cmake --build build
 cmake --build build -- install
